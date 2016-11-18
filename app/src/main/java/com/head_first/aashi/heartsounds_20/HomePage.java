e57ce760cfb5a20d538474e22e59273983ed29b3
Aashish <aashish.si@gmail.com>
2016-11-19 10:15:29 +1300

    Basic overview of Models from the MVC design patters

---
+++ app/src/main/java/com/head_first/aashi/heartsounds_20/HomePage.java
@@ -0,0 +1,13 @@
+package com.head_first.aashi.heartsounds_20;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+    }
+}
